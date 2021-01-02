57b91e2b17a45bdb4bd80192964e5f1d347ce359
Sangman Jung <sangmanjung@khu.ac.kr>
2021-01-03 05:24:07 +0900

    Delete HKRK_WeatherObservationStation15.sql

--- basic/SQL/HKRK_WeatherObservationStation15.sql
+++
@@ -1,3 +0,0 @@
-SELECT round(long_w,4) FROM station
--- choose lat_n that the maximum value of lat_n from station if lat_n < 137.2345.
-WHERE lat_n = (SELECT max(lat_n) FROM station WHERE lat_n < 137.2345)
