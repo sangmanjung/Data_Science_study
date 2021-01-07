b86b0baa25c2d1d6bea38714cc930663e9fbc2f0
Sangman Jung <sangmanjung@khu.ac.kr>
2021-01-07 20:30:01 +0900

    Delete HKRK_TopEarners.sql

--- basic/SQL/HackerRank_practice/HKRK_TopEarners.sql
+++
@@ -1,4 +0,0 @@
-SELECT salary*months as earnings,count(*) FROM employee -- make a group "earnings"
-GROUP BY 1 -- first column of group "earnings"
-ORDER BY 1 DESC -- from the maximum value of "earnings" to the minimum value of that.
-LIMIT 1; -- pick the maximum
