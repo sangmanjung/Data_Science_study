eeec0d87d995cb05a03f20915884056037a9677a
Sangman Jung <sangmanjung@khu.ac.kr>
2021-01-10 01:28:09 +0900

    Update ISNULL.sql

--- SQL_Programming/Programmers_SQL_kit/ISNULL.sql
+++ SQL_Programming/Programmers_SQL_kit/ISNULL.sql
@@ -1,3 +1,18 @@
 -- problem #1 이름이 없는 동물의 아이디
 SELECT animal_id FROM animal_ins
 WHERE isnull(name)
+
+-- problem #2 이름이 있는 동물의 아이디
+SELECT animal_id FROM animal_ins
+WHERE name is not null
+ORDER BY animal_id ASC
+
+-- problem #3 NULL 처리하기
+SELECT animal_type AS 'ANIMAL_TYPE',
+CASE
+    WHEN isnull(name)
+    THEN 'No name'
+    ELSE name
+    END AS 'NAME',
+sex_upon_intake AS 'SEX_UPON_INTAKE' FROM animal_ins
+ORDER BY animal_id
